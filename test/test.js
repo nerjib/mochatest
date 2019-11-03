0a61cb6e26cc9832e3e1f90ee521adb7d352b373
kabirnjib0@gmail.com <kabirnajib0@gmail.com>
2019-11-03 02:41:04 +0100

    full vommit

--- test/test.js
+++ test/test.js
@@ -1,24 +1,28 @@
-var assert = require('assert');
-var fizzbuzz = require('../');
+/* eslint-disable linebreak-style */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+const assert = require('assert');
+const fizzbuzz = require('../');
 
-describe('fizzbuzz', function() {
-  it('returns null when passed a non-number', function() {
+describe('fizzbuzz', function () {
+  it('returns null when passed a non-number', function () {
     assert.equal(fizzbuzz('abc'), null);
   });
 
-  it('returns fizzbuzz when divisible by 15', function() {
+  it('returns fizzbuzz when divisible by 15', function () {
     assert.equal(fizzbuzz(45), 'fizzbuzzz');
   });
 
-  it('returns fizz when divisible by 3 but not 5', function() {
+  it('returns fizz when divisible by 3 but not 5', function () {
     assert.equal(fizzbuzz(6), 'fizz');
   });
 
-  it('returns buzz when divisble by 5 but not 3', function() {
+  it('returns buzz when divisble by 5 but not 3', function () {
     assert.equal(fizzbuzz(10), 'buzz');
   });
 
-  it('returns input otherwise', function() {
+  it('returns input otherwise', function () {
     assert.equal(fizzbuzz(7), '7');
   });
 });
